9f12551049615e44ca9bb9fecdff7df7e0a08e37
Christopher.Goodwin <christopher.goodwin@serko.com>
2023-07-17 16:07:54 +1200

    Create basic search flow

---
+++ src/app/search/page.tsx
@@ -0,0 +1,27 @@
+import * as React from "react";
+import Container from "@mui/material/Container";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+
+export default function StarredPage({
+  searchParams,
+}: {
+  searchParams: { [key: string]: string | string[] | undefined };
+}) {
+  return (
+    <Container>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Typography variant="body1" gutterBottom>
+          {JSON.stringify(searchParams)}
+        </Typography>
+      </Box>
+    </Container>
+  );
+}
